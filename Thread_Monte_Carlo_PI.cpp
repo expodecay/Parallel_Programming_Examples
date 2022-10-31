88c16a430c8ce0306352dc26c97cdff276309c5e
Expodecay <rick.ramirez6174@gmail.com>
2022-10-31 10:14:34 -0700

    Adding timer / delta calculations and setting decimal precision

--- Thread_Monte_Carlo_PI.cpp
+++ Thread_Monte_Carlo_PI.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include <vector>
 #include <thread>
+#include <iomanip>
 
 
 using namespace std;
@@ -13,39 +14,34 @@ using namespace std;
 #define MAX_POINTS 1000000 
 mutex myMutex;
 
+static void printUsage(char** argv) {
+    cout << "usage: monte [1...10] [10...1000000]" << endl;
+}
+
+
 // Command line argument
 int points, thread_count;
 static int parseCmdArgs(int argc, char** argv) {
+    if (argc == 1 | argc > 3) {
+        printUsage(argv);
+        return -1;
+    }
+    if (atof(argv[1]) < 1 | atof(argv[1]) > 10) {
+        printUsage(argv);
+        return -1;
+    }
+    if (atof(argv[2]) < 10 | atof(argv[2]) > 1000000) {
+        printUsage(argv);
+        return -1;
+    }
+    
     thread_count = static_cast<float>(atof(argv[1]));
     points = static_cast<float>(atof(argv[2]));
 
     return 0;
 }
 
-
-//void Calculate_PI(int& interval, double& PI_approx) {
-//    std::cout << "points............ " << points << std::endl;
-//    std::cout << "interval............ " << interval << std::endl;
-//
-//    PI_approx = 3;
-//    //printf("Total points: %d   Interval: %d points per node   PI Approximation: %d\n", points, interval, PI_approx);
-//}
-//
-//int main(int argc, char* argv[]) {
-//
-//    int retval = parseCmdArgs(argc, argv);
-//    int interval = (int)floor(points / thread_count); 
-//    int PI_approx=0;
-//
-//    thread t1(Calculate_PI, ref(interval), ref(PI_approx));
-//    t1.join();
-//    return 0;
-//}
-
-
-
-void calc_pi(int& test_interval, double& PI_approx, int thread_number)  
-{
+void calc_pi(int& test_interval, double& PI_approx, int thread_number) {
     double x, y, dist, local_PI, inside = 0;
     
     srand(time(NULL) + thread_number);
@@ -63,33 +59,21 @@ void calc_pi(int& test_interval, double& PI_approx, int thread_number)
             inside += 1;
         }
     }
-    //cout << "x: " << x << " y" << y << endl;
     local_PI = (double)(4 * inside) / test_interval;
-    printf("Thread[%d] - Approximatex value of Pi: %f\n", thread_number, local_PI);
+
+    printf("Thread[%d] -- Local value of Pi... %f\n", thread_number, local_PI);
     // Critical Section
-    // Add current PI calculation to PI_approximate
+    // Add local PI calculation to PI_approximate
     lock_guard<mutex> myLock(myMutex);
     PI_approx += local_PI;
 }
 
-int main(int argc, char* argv[])
-{ 
-    cout << endl;
+int main(int argc, char* argv[]) { 
     int retval = parseCmdArgs(argc, argv);
     int interval = (int)floor(points / thread_count);
-    double PI_approx = 0;
+    double PI_approx, delta;
 
-    //const int threads = 3; ///// fix this /////////
-    //thread t[threads];
-    //for (int i = 0; i < threads; ++i) {
-    //    t[i] = thread(calc_pi, ref(interval), std::ref(PI_approx), i);
-    //}
-    //
-    //
-    //for (int i = 0; i < threads; ++i) {
-    //    t[i].join();
-    //}
-
+    auto start = chrono::system_clock::now();
     thread* myThreads = new thread[thread_count];
 
     for (int i = 0; i < thread_count; ++i) {
@@ -101,10 +85,17 @@ int main(int argc, char* argv[])
         myThreads[i].join();
     }
 
-    //std::thread t1(calc_pi,ref(interval), std::ref(PI_approx));  
-    // t1.join();
+    PI_approx = PI_approx / thread_count;
+    delta = abs(PI - PI_approx);
+    chrono::duration<double> dur = chrono::system_clock::now() - start;
     cout << endl;
-    cout << "PI approximation: " << PI_approx << endl;
+    
+    std::cout << std::setprecision(4) << std::fixed;
+    cout << "Global Pi approximation..... " << PI_approx << endl;
+    std::cout << std::setprecision(4) << std::fixed;
+    cout << "Delta....................... " << delta << endl;
+    std::cout << std::setprecision(6) << std::fixed;
+    cout << "Compute time................ " << dur.count() << " seconds" << endl;
 
     delete[] myThreads;
     return 0;
